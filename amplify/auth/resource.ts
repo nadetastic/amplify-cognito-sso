b7214a06694590e18881fe964d26eeb2fa07d285
Dan Kiuna <dkkiuna@amazon.com>
2025-08-05 11:31:22 -0500

    initial clone

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,21 @@ import { defineAuth } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: "SSOProvider",
+          clientId: secret("SSOSecret"),
+          clientSecret: secret("FEDERATE_SECRET"),
+          issuerUrl: "https://issuer-url.example.com",
+          attributeMapping: {
+            email: "EMAIL",
+            preferredUsername: "USER_NAME",
+          },
+        },
+      ],
+      callbackUrls: ["http://localhost:5173/k"],
+      logoutUrls: ["http://localhost:5173/logout"],
+    },
   },
 });
